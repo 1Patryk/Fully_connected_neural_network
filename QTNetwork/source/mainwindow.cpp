3937f6a832cf1b62fa6719fa833e848f5a781f3e
Patrick <1patryk.polak@gmail.com>
2024-07-14 19:49:44 +0200

    Repaired puting time in output file path

---
+++ QTNetwork/source/mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+#include "../../include/Fully_connected_network.hpp"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    Fully_connected_network NETWORK;
+
+    std::string Open_filename = {"C:/Users/patry/Desktop/GitHub/Fully_connected_neural_network/Data/Add_1000.txt"};
+    std::string Output_filename_path = {"C:/Users/patry/Desktop/GitHub/Fully_connected_neural_network/Output_data_(MSE)/"};
+    NETWORK.Calculating_the_network_MLP(&Open_filename, &Output_filename_path);
+}
+

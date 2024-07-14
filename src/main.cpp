01468dbbded48909c73ae1f542b30f347b17f558
Patrick <1patryk.polak@gmail.com>
2024-07-14 16:30:43 +0200

    Connecting with QT

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,8 @@
 #include "../Include/Fully_connected_network.hpp"
-#include <QApplication>
-#include <QWidget>
 
 int main(int argc, char* argv[])
 {
 	auto Start = std::chrono::high_resolution_clock::now();
-
-	QApplication app(argc, argv);
-	QWidget Window;
-
-	Window.resize(320, 240);
-	Window.show();
-	Window.setWindowTitle(
-		QApplication::translate("toplevel", "Top-level widget"));
 
 	Fully_connected_network DATA;				
 
@@ -22,6 +12,4 @@ int main(int argc, char* argv[])
 
 	auto Stop = std::chrono::high_resolution_clock::now();
 	DATA.Display_results_counting_time(Start, Stop, "Cumulatively", 2);
-
-	return app.exec();
 }

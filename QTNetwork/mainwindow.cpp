8c3a00e9035f9b45547f982ed03aa887d8df30a8
Patric <136099605+1Patryk@users.noreply.github.com>
2024-09-24 19:21:03 +0200

    Adding cmake features and cpack

--- QTNetwork/mainwindow.cpp
+++ QTNetwork/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QDebug>
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -19,10 +20,23 @@ void MainWindow::on_pushButton_settings_clicked()
     qDebug() << "You pressed settings button";
 }
 
-
-
 void MainWindow::on_pushButton_clicked()
 {
-
+    QFile file("Add_1000.txt");
+    if(!file.open(QIODevice::ReadOnly))
+    {
+        ui->textEdit->setText("Nie znaleziono pliku z pytaniami.");
+    }
+    else
+    {
+        QString line;
+        QTextStream stream(&file);
+        while(!stream.atEnd())
+        {
+            line=stream.readLine();
+            ui->textEdit->setText(line);
+        }
+    }
+    file.close();
 }
 
